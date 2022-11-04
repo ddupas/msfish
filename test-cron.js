1741a95e4840a9fb033720798ab6e0f28997cac0
Darrell Dupas <dirtslayer@gmail.com>
2022-11-04 17:28:32 -0600

    dd

--- test-cron.js
+++ test-cron.js
@@ -2,7 +2,7 @@ const schedule = require('node-schedule');
 const { snapshotall } = require('./snapshotall');
 const { updateplayers} = require('./updateplayers');
 
-const snapshotjob = schedule.scheduleJob('56 5,8,11,15,19,23 * * *',async function(){
+const snapshotjob = schedule.scheduleJob('56 5,8,11,14,17,20,23 * * *',async function(){
   // https://crontab.guru/#56_7,11,15,19,23_*_*_*
   await snapshotall();
   console.log(`test-cron snapshotall: ${new Date()}`);
