e55dfca2d3bdd5b674db54b7e6ef961e5471c9a5
Darrell D <darrell@darrelldupas.info>
2022-11-25 13:08:52 -0700

    dd

---
+++ pm2/ecosystem.config.js
@@ -0,0 +1,17 @@
+/*
+install command:
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u darrell --hp /home/darrell/msfish/
+start command:
+pm2 start ecosystem.config.js
+pm2 save
+*/
+
+module.exports = {
+  apps : [{
+    cwd: '/home/darrell/msfish/',
+    script: 'index.js',
+  }, {
+    cwd: '/home/darrell/msfish/',
+    script: '/home/darrell/msfish/test-cron.js',
+  }]
+};
