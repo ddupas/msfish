0389d65b578455336f289f26e1e2fb35d97b6ea7
Darrell D <darrell@darrelldupas.info>
2022-11-25 15:04:54 -0700

    dd

--- test-snapshot.js
+++ test-snapshot.js
@@ -2,4 +2,7 @@
 
 const { snapshotone } = require('./snapshotall');
 
-snapshotone('62d023c9d142af213c5def32');
+//snapshotone('62d023c9d142af213c5def32');
+
+// Milan
+snapshotone('60a6a302d142af1f1d389c83'); 
