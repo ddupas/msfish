a54487e460ecb560b937dc6ca4d6f3f538a427f1
Darrell D <darrell@darrelldupas.info>
2022-11-25 15:05:35 -0700

    dd

--- .pm2/ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apps : [{
-    cwd: '/home/darrell/msfish/',
-    script: 'index.js',
-  }, {
-    cwd: '/home/darrell/msfish/',
-    script: '/home/darrell/msfish/test-cron.js',
-  }]
-};
