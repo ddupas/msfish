8082bad42197509e3b2f6749eead3c62433cae17
Darrell D <darrell@darrelldupas.info>
2024-03-24 17:50:53 -0600

    es6ify

---
+++ deletedup.mjs
@@ -0,0 +1,22 @@
+'use strict';
+
+import { promisify } from 'node:util';
+import { exec } from 'node:child_process'
+
+const promise_exec = promisify(exec);
+
+function log(l) {
+	console.log('deletedup: ' + l);
+}
+
+export async function deletedup() {
+	try {
+		log(new Date().toLocaleString());
+		const { stdout, stderr } = await
+		await promise_exec('cat dd.sql | sqlite3 msfish.db');
+		log('successful delete duplicates');
+	}
+	catch (e) {
+		log('error ' + e);
+	}
+}
---
+++ emoji.mjs
@@ -0,0 +1,27 @@
+'use strict';
+
+import { random } from 'node-emoji';
+
+export function re() {
+	return random().emoji;
+}
+
+const abc = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭',
+	'🇮', '🇯', '🇰', '🇱', '🇲', '🇳', '🇴', '🇵', '🇶', '🇷',
+	'🇸', '🇹', '🇺', '🇻', '🇼', '🇽', '🇾', '🇿'];
+
+export function ri(sentance = 'puppy') {
+	// abc['c'.charCodeAt()-'a'.charCodeAt()]
+	let toret = '';
+	const lowerstr = sentance.toLowerCase();
+	[...lowerstr].forEach((character) => {
+		const i = character.charCodeAt() - 'a'.charCodeAt();
+		if (i >= 0 && i < 26) {
+			toret += abc[i] + ' ';
+		}
+		else {
+			toret += character;
+		}
+	});
+	return toret;
+}
