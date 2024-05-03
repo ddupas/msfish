165da0b393da99b0f3d3cc35838925a0bdc7df14
Darrell D <darrell@darrelldupas.info>
2024-05-03 15:29:17 -0600

    dd

--- tests/test-gitpushdb.js
+++ tests/test-gitpushdb.js
@@ -1,18 +1,15 @@
-const { pushdb,pulldb } = require('../gitpushdb');
-const schedule = require('node-schedule');
-const  { parseArgs } = require('node:util');
+const { pushdb } = require('../gitpushdb.mjs');
+import { exit } from 'node:process';
 
-try {
-	pulldb();
-} catch (e) {
-        console.log(e);
+function log(l) {
+	console.log('test gitpushdb: ' + l);
 }
 
-schedule.scheduleJob('4,19,24,29,34,39,44,49,54,59 * * * *', async function() {
-	try {
-		await pushdb();
-	}
-	catch (e) {
-		console.log('test-gitpushdb push fail ' + e);
-	}
-});
+try {
+	await pushdb();
+}
+catch (e) {
+	log('push fail ' + e);
+	exit(1);
+}
+log('push success')
